2fb0f3c33c7070d8c0851876d0c98a6985447f9d
tyhenkel <tyhenkel+dev@gmail.com>
2021-08-06 13:04:31 -0600

    fix path

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
 import get from 'lodash/get';
 import { graphql } from 'gatsby';
 
